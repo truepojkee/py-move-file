384946888feec6b04dd737b607ff1e5f1932bd61
Pojkee <cbo6oda22@gmail.com>
2026-05-03 17:04:13 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,24 @@
 import os
 
-
 def move_file(command: str) -> None:
     words_list = command.split()
-    destination = words_list[2]
-    path = destination.split("/")
-
-    if words_list[0] != "mv":
+    if words_list[0] != "mv" or len(words_list) != 3:
         return
-
-    current_path = ""
-    for folder in path[:-1]:
-        current_path = os.path.join(current_path, folder)
-        if not os.path.exists(current_path):
-            os.mkdir(current_path)
-
-    if (len(words_list) == 3 and words_list[0] == "mv"
-            and words_list[1] != words_list[2]):
-        os.rename(words_list[1], words_list[2])
+    source, destination = words_list[1], words_list[2]
+    if destination.endswith("/"):
+        destination = os.path.join(destination, os.path.basename(source))
+    dest_dir = os.path.dirname(destination)
+    if dest_dir:
+        current_path = ""
+        for folder in dest_dir.split("/"):
+            if current_path == "":
+                current_path = folder
+            else:
+                current_path = current_path + "/" + folder
+            if not os.path.exists(current_path):
+                os.mkdir(current_path)
+    with open(source, "r") as file:
+        content = file.read()
+    with open(destination, "w") as file:
+        file.write(content)
+    os.remove(source)
